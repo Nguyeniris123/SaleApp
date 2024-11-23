be205a80d8433f77ec8e92b3ceae7d1ed558ca51
nguyeniris123 <nguyen.hochi2004@gmail.com>
2024-11-23 16:17:02 +0700

    Đăng kí đăng nhập

--- saleapp/app/dao.py
+++ saleapp/app/dao.py
@@ -1,92 +1,21 @@
 from app.models import Category, Product
+from app import app
 
 def load_categories():
     return Category.query.order_by('id').all()
-    # return [{
-    #     'id': 1,
-    #     'name': 'mobile'
-    # }, {
-    #     'id': 2,
-    #     'name': 'Table'
-    # }]
 
-def load_products(cate_id=None, kw=None):
+def load_products(cate_id=None, kw=None, page=1):
     query = Product.query
     if kw:
         query = query.filter(Product.name.contains(kw))
     if cate_id:
         query = query.filter(Product.category_id == cate_id)
+
+    page_size = app.config.get('PAGE_SIZE')
+    start = (page - 1) * page_size
+    query = query.slice(start, start + page_size)
     return query.all()
-    # return [{
-    #     "name": "iPhone 7 Plus",
-    #     "description": "Apple, 32GB, RAM: 3GB, iOS13",
-    #     "price": 17000000,
-    #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647056401/ipmsmnxjydrhpo21xrd8.jpg",
-    #     "category_id": 1
-    # }, {
-    #     "name": "iPad Pro 2020",
-    #     "description": "Apple, 128GB, RAM: 6GB",
-    #     "price": 37000000,
-    #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg",
-    #     "category_id": 2
-    # }, {
-    #     "name": "Galaxy Note 10 Plus",
-    #     "description": "Samsung, 64GB, RAML: 6GB",
-    #     "price": 24000000,
-    #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
-    #     "category_id": 1
-    # }, {
-    #     "name": "iPhone 7 Plus",
-    #     "description": "Apple, 32GB, RAM: 3GB, iOS13",
-    #     "price": 17000000,
-    #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647056401/ipmsmnxjydrhpo21xrd8.jpg",
-    #     "category_id": 1
-    # }, {
-    #     "name": "iPad Pro 2020",
-    #     "description": "Apple, 128GB, RAM: 6GB",
-    #     "price": 37000000,
-    #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg",
-    #     "category_id": 2
-    # }, {
-    #     "name": "Galaxy Note 10 Plus",
-    #     "description": "Samsung, 64GB, RAML: 6GB",
-    #     "price": 24000000,
-    #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
-    #     "category_id": 1
-    # }, {
-    #     "name": "iPhone 7 Plus",
-    #     "description": "Apple, 32GB, RAM: 3GB, iOS13",
-    #     "price": 17000000,
-    #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647056401/ipmsmnxjydrhpo21xrd8.jpg",
-    #     "category_id": 1
-    # }, {
-    #     "name": "iPad Pro 2020",
-    #     "description": "Apple, 128GB, RAM: 6GB",
-    #     "price": 37000000,
-    #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg",
-    #     "category_id": 2
-    # }, {
-    #     "name": "Galaxy Note 10 Plus",
-    #     "description": "Samsung, 64GB, RAML: 6GB",
-    #     "price": 24000000,
-    #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
-    #     "category_id": 1
-    # }, {
-    #     "name": "iPhone 7 Plus",
-    #     "description": "Apple, 32GB, RAM: 3GB, iOS13",
-    #     "price": 17000000,
-    #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647056401/ipmsmnxjydrhpo21xrd8.jpg",
-    #     "category_id": 1
-    # }, {
-    #     "name": "iPad Pro 2020",
-    #     "description": "Apple, 128GB, RAM: 6GB",
-    #     "price": 37000000,
-    #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg",
-    #     "category_id": 2
-    # }, {
-    #     "name": "Galaxy Note 10 Plus",
-    #     "description": "Samsung, 64GB, RAML: 6GB",
-    #     "price": 24000000,
-    #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
-    #     "category_id": 1
-    # }]
+
+def count_products():
+    return Product.query.count()
+
--- saleapp/app/index.py
+++ saleapp/app/index.py
@@ -1,3 +1,4 @@
+import math
 from flask import render_template, request
 import dao
 from app import app
@@ -8,8 +9,16 @@ def index():
     cates = dao.load_categories()
     cate_id = request.args.get('category_id')
     kw = request.args.get('kw')
-    pruds = dao.load_products(cate_id=cate_id, kw=kw) # cách lấy dữ liệu phổ biến
-    return render_template('index.html', categories=cates, products=pruds)
+    page = request.args.get('page', 1)
+    prods = dao.load_products(cate_id=cate_id, kw=kw, page=int(page)) # cách lấy dữ liệu phổ biến
+
+    page_size = app.config.get('PAGE_SIZE', 8)
+    total = dao.count_products()
+    return render_template('index.html', categories=cates, products=prods, pages=math.ceil(total/page_size))
+
+@app.route("/login")
+def login_process():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
